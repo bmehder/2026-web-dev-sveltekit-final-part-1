deaa35041fa1cb769d60d453bdbf14ab7edf3da4
bmehder <bmehder@gmail.com>
2026-04-13 16:44:50 +0100

    dynamic routes

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,17 @@
+<script>
+	let { data } = $props()
+</script>
+
 <h1>Blog</h1>
+
+{#each data.posts as post}
+	<article>
+		<a href="/blog/{post.slug}"><img src={post.meta.image} alt="" /></a>
+		<h3>
+			<a href="/blog/{post.slug}">{post.meta.title}</a>
+		</h3>
+		<div>{post.meta.date}</div>
+		<p>{post.meta.excerpt}</p>
+		<a href="/blog/{post.slug}">Read More</a>
+	</article>
+{/each}
---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  let { data } = $props()
+
+  let Post = $derived(data.post.component)
+</script>
+
+<article>
+  <img src="/{data.post.meta.image}" alt="">
+  <h1>{data.post.meta.title}</h1>
+  <div>{data.post.meta.date}</div>
+  <Post></Post>
+</article>
