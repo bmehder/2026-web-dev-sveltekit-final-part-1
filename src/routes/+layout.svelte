6b35d7b4751e51abb129d2a1a440a3f042da9b43
bmehder <bmehder@gmail.com>
2026-04-13 01:29:28 +0100

    first

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '$lib/style.css'
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<a href="/">Home</a>
+<a href="/about/">About</a>
+<a href="/blog/">Blog</a>
+<a href="/contact/">Contact</a>
+
+{@render children()}
